ee13c0700ab075c94e4b2d6cccd729083f8676bc
viraaelka <aelya089@gmail.com>
2019-02-15 00:06:19 +0300

    The working option

--- src/panel/Calculation.java
+++ src/panel/Calculation.java
@@ -4,17 +4,13 @@ import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Calculation {
-
-
-    public static void main(String[]args){
-        calcHex("5C9", 2);
-
+    public static void main(String[] args) {
+        calcOct("526", 10);
     }
-
-    public static String[] binMassive = {"0000", "0001","0010","0011", "0100", "0101", "0110", "0111", "1000", "1001"};
+    public static String[] binMassive = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001"};
     public static HashMap<String, String> binHex = new HashMap<>();
+    public static String[] letterMas = {"A", "B", "C", "D", "E", "F"};
     static {
         binHex.put("A", "1010");
         binHex.put("B", "1011");
@@ -23,27 +19,14 @@ public class Calculation {
         binHex.put("E", "1110");
         binHex.put("F", "1111");
     }
-    public static HashMap<String, String> letterToNumb = new HashMap<>();
-    public static String[] letterMas = {"A", "B", "C", "D", "E", "F"};
-    static {
-        letterToNumb.put("10", "A");
-        letterToNumb.put("11", "B");
-        letterToNumb.put("12", "C");
-        letterToNumb.put("13", "D");
-        letterToNumb.put("14", "E");
-        letterToNumb.put("15", "F");
-    }
-
-   // public static String[] numbHex = {"A", "B", "C", "D", "E", "F"};
-
-    public static void  calculation(String k, int temp) {
-
+    // public static String[] numbHex = {"A", "B", "C", "D", "E", "F"};
+    public static void calculation(String k, int temp) {
       /*  if () calcDec();
             if () calcHex();
                 if () calcOct();
                     if () */
     }
-    public static void calcBin(String k, int temp){
+    public static void calcBin(String k, int temp) {
         String rec = ""; // sum = "";
         int number = 0;
         try {
@@ -56,69 +39,56 @@ public class Calculation {
                 rec = Convertion.convertationNumbToBin(number);
                 System.out.println("rec " + rec);
             }
-
-        }catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             if (temp == 16) {
                 for (Character c : k.toCharArray()) {
-                    if(Character.isDigit(c)){
+                    if (Character.isDigit(c)) {
                         rec += Convertion.convertationNumbToBin(Character.getNumericValue(c)) + "";
-                    }else{
-                         for (Map.Entry<String, String> pair : binHex.entrySet()) {
-                             if (String.valueOf(c).equals(pair.getKey()))
-                                 rec += pair.getValue() + "";
-                         }
-                      }
+                    } else {
+                        for (Map.Entry<String, String> pair : binHex.entrySet()) {
+                            if (String.valueOf(c).equals(pair.getKey()))
+                                rec += pair.getValue() + "";
+                        }
+                    }
                 }
             }
             System.out.println("rec_Hex " + rec);
         }
     }
-
-
-    public static void calcOct(String k, int temp){
+    public static void calcOct(String k, int temp) {
         String rec = "", sum = "";
-        if(temp == 2){
+        if (temp == 2) {
             rec = Convertion.convertToBin(k);
             System.out.println("recOct = " + rec);
         }
-        int number = 0;
-        try{
-            number = Integer.parseInt(k);
-            if(temp == 10){
-              rec = Convertion.divisionToNumb(number, 8);
-                System.out.println("recOct = " + rec);
-            }
-        }
-        catch(NumberFormatException e) {
-            System.out.println("Wrong convertion to octavian");
+        if (temp == 10) {
+            rec = Convertion.multiplyMethod(k, 8);
+            System.out.println("rec: " + rec);
         }
         if (temp == 16) {
-                  for(Character ch : k.toCharArray()) {
-                      if(!Character.isDigit(ch)) {
-                          for (Map.Entry<String, String> pair : binHex.entrySet()) {
-                              if (pair.getKey().equals(String.valueOf(ch)))
-                                  rec = rec + pair.getValue();
-                          }
-                      }
-                      else {
-                        for (int i = 0; i < binMassive.length; i++) {
-                            if (Character.getNumericValue(ch) == i)
-                                rec += binMassive[i];
-                        }
+            for (Character ch : k.toCharArray()) {
+                if (!Character.isDigit(ch)) {
+                    for (Map.Entry<String, String> pair : binHex.entrySet()) {
+                        if (pair.getKey().equals(String.valueOf(ch)))
+                            rec = rec + pair.getValue();
                     }
+                } else {
+                    for (int i = 0; i < binMassive.length - 2; i++) {
+                        if (Character.getNumericValue(ch) == i)
+                            rec += binMassive[i];
+                    }
+                }
             }
-                sum = Convertion.convertToBin(rec);
+            sum = Convertion.convertToBin(rec);
             System.out.println("sumHex = " + sum);
         }
     }
     public static void calcHex(String k, int temp) {
         String rec = "", sum = "";
         int number = 0, h = 0;
-
-        //  number = Integer.parseInt(k);
         if (temp == 10) {
             try {
-                //     number = Integer.parseInt(k);
+                number = Integer.parseInt(k);
                 while (number > 0) {
                     h = number % 16;
                     if (number % 16 >= 10) {
@@ -131,30 +101,31 @@ public class Calculation {
                     }
                     number = number / 16;
                 }
-                for (int i = rec.length() - 1; i >= 0; i--) {
-                    sum += rec.charAt(i) + "";
-                }
+                sum = Convertion.returnOperation(rec);
                 System.out.println("recHex = " + sum);
             } catch (NumberFormatException e) {
                 System.out.println("Wrong value is detected in calcHex");
             }
         }
         if (temp == 2) {
-            if (k.length() % 4 != 0) {
-                for (int i = k.length() % 4; i >= 0; i--) {
-                    k = "0" + k;
-                }
-            }
+            k = Convertion.zeroAddition(k, 4);
             for (int i = 0; i < k.length(); i += 4) {
                 rec = k.substring(i, i + 4);
-                for (int j = 0; j < binMassive.length; j++) {
-                    if (rec.equals(binMassive[j].substring(0, 4)))
+                for (int j = 0; j < Calculation.binMassive.length; j++) {
+                    if (rec.equals(Calculation.binMassive[j].substring(0, 4)))
                         sum += j;
                 }
+                for (Map.Entry<String, String> pair : binHex.entrySet()) {
+                    if (pair.getValue().equals(rec))
+                        sum += pair.getKey();
+                }
             }
+            System.out.println("recHex = " + sum);
+        }
+        if (temp == 8) {
+            rec = Convertion.multiplyMethod(k, 8);
+            calcHex(rec, 10);
         }
     }
-    }
-
-
+}
 
--- src/panel/Convertion.java
+++ src/panel/Convertion.java
@@ -7,12 +7,9 @@ public class Convertion {
             rec += String.valueOf(number % temp) + "";
             number = number / temp;
         }
-        for (int i = rec.length() - 1; i >= 0; i--) {
-            sum += rec.charAt(i) + "";
-        }
+        sum = returnOperation(rec);
         return sum;
     }
-
     public static String convertationNumbToBin(int number){
         String rec = "";
         for (Character charac : String.valueOf(number).toCharArray()) {
@@ -25,20 +22,50 @@ public class Convertion {
     }
     public static String convertToBin(String k){
         String rec = "", sum = "";
-        int m = k.length()%3;
-        int h = 3 - k.length()%3;
-        if(k.length()%3 != 0){
-            for(int i = (3 - k.length()%3); i > 0; i--){
-                k = "0" + k;
-            }
-        }
+        k = zeroAddition(k, 3);
+
         for(int i = 0; i < k.length(); i+=3){
             rec = k.substring(i, i+3);
-            for (int j = 0; j < Calculation.binMassive.length-2; j++){
-                if(rec.equals(Calculation.binMassive[j].substring(1,4)))
+            for (int j = 0; j < Calculation.binMassive.length; j++){
+                if (rec.equals(Calculation.binMassive[j].substring(1, 4)))
                     sum += j;
             }
         }
         return sum;
     }
+
+    public static String zeroAddition(String kup, int t){
+        if(kup.length()%t != 0){
+            for(int i = t - kup.length()%t; i >= 0; i--){
+                kup = "0" + kup;
+            }
+        }
+        return kup;
+    }
+    public static String returnOperation(String rec){
+        String sum = "";
+        for (int i = rec.length() - 1; i >= 0; i--) {
+            sum += rec.charAt(i) + "";
+        }
+        return sum;
+    }
+    public static String multiplyMethod(String str, int temp){
+        int number = 0, d = 0, p = 0, m = 0, result = 0;
+        String sum = "", res = "";
+        int index = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                for (int j = 0; j < Calculation.letterMas.length; j++) {
+                    if (String.valueOf(str.charAt(i)).equals(Calculation.letterMas[j]))
+                        number = Integer.parseInt("1" + j);
+                }
+            } else {
+                number = Integer.parseInt(String.valueOf(str.charAt(i)));
+            }
+            d = (int) Math.pow(temp, str.length()-i-1);
+            result += d * number;
+        }
+        return String.valueOf(result);
+    }
 }
+
